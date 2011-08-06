feb78b35462b2867a49f70d39624a9b703776011
alrocar <caldodeave@gmail.com>
2011-08-06 21:52:05 +0200

    POIProxy REST service using RESTlet

---
+++ es.alrocar.poiproxy.rest/src/main/java/es/alrocar/poiproxy/rest/BrowsePOIProxyZXY.java
@@ -0,0 +1,92 @@
+/* POIProxy
+ *
+ * Copyright (C) 2011 Alberto Romeu.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,USA.
+ *
+ * For more information, contact:
+ *
+ *   Prodevelop, S.L.
+ *   Pza. Don Juan de Villarrasa, 14 - 5
+ *   46001 Valencia
+ *   Spain
+ *
+ *   +34 963 510 612
+ *   +34 963 510 968
+ *   prode@prodevelop.es
+ *   http://www.prodevelop.es
+ *   
+ *   2011.
+ *   author Alberto Romeu aromeu@prodevelop.es  
+ *   
+ */
+
+package es.alrocar.poiproxy.rest;
+
+import java.util.Calendar;
+import java.util.HashMap;
+
+import org.restlet.Context;
+import org.restlet.Request;
+import org.restlet.Response;
+import org.restlet.data.Form;
+import org.restlet.data.MediaType;
+import org.restlet.data.Parameter;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.representation.Variant;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.ServerResource;
+
+import es.alrocar.poiproxy.proxy.POIProxy;
+
+public class BrowsePOIProxyZXY extends ServerResource {
+
+	public BrowsePOIProxyZXY() {
+		super();
+	}
+
+	public BrowsePOIProxyZXY(Context context, Request request, Response response) {
+		getVariants().add(new Variant(MediaType.TEXT_PLAIN));
+	}
+
+	@Override
+	protected Representation get() throws ResourceException {
+		Request r = this.getRequest();
+		Form form = r.getResourceRef().getQueryAsForm();
+		HashMap<String, String> params = new HashMap<String, String>();
+		for (Parameter parameter : form) {
+			params.put(parameter.getName(), parameter.getValue());
+		}
+
+		POIProxy proxy = POIProxy.getInstance();
+
+		proxy.initialize();
+
+		String geoJSON = "";
+		try {
+			geoJSON = proxy.getPOIs(params.get("service"),
+					Integer.valueOf(params.get("z")),
+					Integer.valueOf(params.get("x")),
+					Integer.valueOf(params.get("y")), null);
+			System.out.println(geoJSON);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return new StringRepresentation(geoJSON, MediaType.APPLICATION_JSON);
+	}
+
+}
---
+++ es.alrocar.poiproxy.rest/src/main/java/es/alrocar/poiproxy/rest/POIProxyApplication.java
@@ -0,0 +1,99 @@
+/* POIProxy
+ *
+ * Copyright (C) 2011 Alberto Romeu.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,USA.
+ *
+ * For more information, contact:
+ *
+ *   Prodevelop, S.L.
+ *   Pza. Don Juan de Villarrasa, 14 - 5
+ *   46001 Valencia
+ *   Spain
+ *
+ *   +34 963 510 612
+ *   +34 963 510 968
+ *   prode@prodevelop.es
+ *   http://www.prodevelop.es
+ *   
+ *   2011.
+ *   author Alberto Romeu aromeu@prodevelop.es  
+ *   
+ */
+
+package es.alrocar.poiproxy.rest;
+
+import org.restlet.Application;
+import org.restlet.Context;
+import org.restlet.Request;
+import org.restlet.Response;
+import org.restlet.Restlet;
+import org.restlet.data.MediaType;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.routing.Router;
+
+public class POIProxyApplication extends Application {
+
+	public POIProxyApplication() {
+		super();
+	}
+
+	public POIProxyApplication(Context parentContext) {
+		super(parentContext);
+	}
+
+	public Restlet createRoot() {
+		Router router = new Router(getContext());
+
+		router.attach("/hello", HelloResource.class);
+		router.attach("/browse", BrowsePOIProxyZXY.class);
+
+		Restlet mainpage = new Restlet() {
+			@Override
+			public void handle(Request request, Response response) {
+				StringBuilder stringBuilder = new StringBuilder();
+
+				stringBuilder.append("<html>");
+				stringBuilder.append("<head><title>Hello Application "
+						+ "Servlet Page</title></head>");
+				stringBuilder.append("<body bgcolor=white>");
+				stringBuilder.append("<table border=\"0\">");
+				stringBuilder.append("<tr>");
+				stringBuilder.append("<td>");
+				stringBuilder.append("<h3>available REST calls</h3>");
+				stringBuilder
+						.append("<ol><li><a href=\"poiproxy/hello\">hello</a> --> returns hello world message "
+								+ "and date string</li>");
+				stringBuilder
+						.append("<li><a href=\"poiproxy/browse?service=panoramio&z=17&x=65397&y=49868\">browse sample</a> --> returns a test panoramio response"
+								+ "</li>");
+
+				stringBuilder.append("</ol>");
+				stringBuilder.append("</td>");
+				stringBuilder.append("</tr>");
+				stringBuilder.append("</table>");
+				stringBuilder.append("</body>");
+				stringBuilder.append("</html>");
+
+				response.setEntity(new StringRepresentation(stringBuilder
+						.toString(), MediaType.TEXT_HTML));
+			}
+		};
+		router.attach("", mainpage);
+
+		return router;
+	}
+
+}
