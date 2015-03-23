0799fdf9bfdb1c41d7bce201c3c0888e32e5c3bf
Alberto Romeu <aromeu@prodevelop.es>
2015-03-23 10:20:44 +0100

    fixed URL endpoind

--- test/docker_smoketest.sh
+++ test/docker_smoketest.sh
@@ -14,7 +14,7 @@ echo "Entering FIC2Lab smoke test sequence. Vendor's validation procedure of $SE
 
 echo "Run smoke test for '$SE is up and running'"
 
-ITEM_RESULT=`curl -s -o /dev/null -w "%{http_code}" http://localhost:8080/poiproxy/describeServices`
+ITEM_RESULT=`curl -s -o /dev/null -w "%{http_code}" http://$HOST:$BASEPORT/poiproxy/describeServices`
 if [ "$ITEM_RESULT" -ne "200" ]; then
     echo "Curl command for '$SE is up and running' failed. Validation procedure terminated."
     echo "Debug information: HTTP code $ITEM_RESULT instead of expected 200 from $HOST"
