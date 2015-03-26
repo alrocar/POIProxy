2a1b90915fc627111f250c354f69b5ede75d1023
Alberto Romeu <aromeu@prodevelop.es>
2015-03-26 09:44:06 +0100

    more verbose smoketest

--- test/docker_smoketest.sh
+++ test/docker_smoketest.sh
@@ -14,13 +14,14 @@ echo "Entering FIC2Lab smoke test sequence. Vendor's validation procedure of $SE
 
 echo "Run smoke test for '$SE is up and running'"
 
-ITEM_RESULT=`curl -s -o /dev/null -w "%{http_code}" http://$HOST:$BASEPORT/poiproxy/describeServices`
+REQUEST_URL=/poiproxy/describeServices
+ITEM_RESULT=`curl -s -o /dev/null -w "%{http_code}" http://$HOST:$BASEPORT$REQUEST_URL`
 if [ "$ITEM_RESULT" -ne "200" ]; then
-    echo "Curl command for '$SE is up and running' failed. Validation procedure terminated."
+    echo "Curl command for $REQUEST_URL failed. Can't get list of available services. Validation procedure terminated."
     echo "Debug information: HTTP code $ITEM_RESULT instead of expected 200 from $HOST"
     exit 1;
 else
-    echo "Curl command for '$SE is up and running' OK."
+    echo "Curl command for $REQUEST_URL OK. A list of services available in $SE has been retrieved"
 fi
 
 echo "Smoke test completed. Vendor component validation procedure succeeded. Over."
