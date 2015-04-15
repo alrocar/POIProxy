b21b4f0c566137de879bc2d05f0999fb6cf767ee
Alberto Romeu <aromeu@prodevelop.es>
2015-04-15 12:35:00 +0200

    Added license headers

--- es.alrocar.poiproxy.fiware.poidp/src/main/java/es/alrocar/poiproxy/fiware/poidp/schema/fw_time_class.java
+++ es.alrocar.poiproxy.fiware.poidp/src/main/java/es/alrocar/poiproxy/fiware/poidp/schema/fw_time_class.java
@@ -1,3 +1,36 @@
+/*
+ * Licensed to Prodevelop SL under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The Prodevelop SL licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * 
+ * For more information, contact:
+ *
+ *   Prodevelop, S.L.
+ *   Pza. Don Juan de Villarrasa, 14 - 5
+ *   46001 Valencia
+ *   Spain
+ *
+ *   +34 963 510 612
+ *   +34 963 510 968
+ *   prode@prodevelop.es
+ *   http://www.prodevelop.es
+ * 
+ * @author Alberto Romeu Carrasco aromeu@prodevelop.es
+ */
+
 package es.alrocar.poiproxy.fiware.poidp.schema;
 
 import es.alrocar.poiproxy.fiware.poidp.schema.utils.Schedule;
@@ -5,47 +38,52 @@ import es.alrocar.poiproxy.fiware.poidp.schema.utils.Source;
 import es.alrocar.poiproxy.fiware.poidp.schema.utils.UpdateStamp;
 
 public class fw_time_class {
-	
-	
+
 	private String type = "open"; // "open", "show_time
 	private String time_zone = "time_zone"; // not decided
-	
+
 	private Schedule schedule = null;
 	private Source source = null;
 	private UpdateStamp last_update = null;
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public String getTime_zone() {
 		return time_zone;
 	}
+
 	public void setTime_zone(String time_zone) {
 		this.time_zone = time_zone;
 	}
+
 	public Schedule getSchedule() {
 		return schedule;
 	}
+
 	public void setSchedule(Schedule schedule) {
 		this.schedule = schedule;
 	}
+
 	public Source getSource() {
 		return source;
 	}
+
 	public void setSource(Source source) {
 		this.source = source;
 	}
+
 	public UpdateStamp getLast_update() {
 		return last_update;
 	}
+
 	public void setLast_update(UpdateStamp last_update) {
 		this.last_update = last_update;
 	}
-	
-	
-	
-	
 
 }
